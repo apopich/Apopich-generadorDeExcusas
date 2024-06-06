5417fb7a4d71a6e456ee855db0c73dde4d34afae
apopich <78308504+apopich@users.noreply.github.com>
2024-06-06 21:44:01 +0000

    generador de excusas Andrea

--- src/app.js
+++ src/app.js
@@ -1,11 +1,33 @@
-/* eslint-disable */
-import "bootstrap";
-import "./style.css";
-
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
-window.onload = function() {
+window.onload = () => {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  //console.log("Hello Rigo from the console!");
+
+  const elementoExcusa = document.getElementById("excusa");
+  elementoExcusa.innerHTML = generarExcusa();
 };
+
+function generarExcusa() {
+  let quien = [
+    "El perro",
+    "Mi hermanito querido",
+    "El vecino",
+    "Mi amigo",
+    "El loro"
+  ];
+  let accion = ["vendio", "rompio", "regalo", "comio", "quemo"];
+  let que = ["mi compu", "la heladera", "el vaso", "la pared", "la cerveza"];
+  let cuando = [
+    "antes de dormir",
+    "demadrugada",
+    "mientras pensaba",
+    "en el gimnasio",
+    "escuchando musica"
+  ];
+
+  let quienIndex = Math.floor(Math.random() * quien.length);
+  let accionIndex = Math.floor(Math.random() * accion.length);
+  let queIndex = Math.floor(Math.random() * que.length);
+  let cuandoIndex = Math.floor(Math.random() * cuando.length);
+
+  return `${quien[quienIndex]} ${accion[accionIndex]} ${que[queIndex]} ${cuando[cuandoIndex]}`;
+}
